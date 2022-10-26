8158d1f7bfeffc6bb79316aa5d18fe1a50715932
VascoMiguens <v_miguens@hotmail.com>
2022-10-26 21:53:41 +0100

    - no margin or padding to rowContainer with no-gutters - added border to hourSlot - added function that updates the task slots with color depending on the time of the day

--- assets/script.js
+++ assets/script.js
@@ -1,39 +1,68 @@
+//variable object with the sart and end of the time slots
 var options = {
   startHour: 1,
   endHour: 24,
 };
 
 function generateTimeSlots() {
+  //loop through every hour and generate the layout
   for (var hour = options.startHour; hour <= options.endHour; hour++) {
+    //create a row container for each time slot
     var rowContainer = $("<div>").addClass(
-      "row time-block d-flex justify-content-center"
+      "row no-gutters time-block d-flex justify-content-center"
     );
+    //give a data-hour attribute
     rowContainer.attr("data-hour", hour);
 
+    //time slot
     var hourSlot = $("<div>")
-      .addClass("col-1 col-sm-1 col-md-1 col-xl-1 d-flex align-items-center")
+      .addClass(
+        "col-1 col-sm-1 col-md-1 col-xl-1 d-flex align-items-center justify-content-center border-top border-dark"
+      )
       .text(moment(hour, "h").format("h A"));
+    //task container
     var taskContainer = $("<div>").addClass(
-      "col-8 col-sm-6 col-md-10 col-xl-10"
+      "col-8 col-sm-10 col-md-10 col-xl-10"
     );
-    taskSlot = $("<textarea>").addClass("col-md-12 description");
+    //task description
+    var taskSlot = $("<textarea>").addClass("col-md-12 description h-100");
+    //save button container
     var saveContainer = $("<div>").addClass(
       "saveBtn d-flex justify-content-center align-items-center col-1 col-sm-1 col-md-1 col-xl-1"
     );
+    //save button
     var saveBtn = $("<i>").addClass("fas fa-save");
 
+    //append elements to parents
     rowContainer.append(hourSlot);
     rowContainer.append(taskContainer);
     taskContainer.append(taskSlot);
     rowContainer.append(saveContainer);
     saveContainer.append(saveBtn);
-
+    //append the row container to the main container
     $(".container").append(rowContainer);
   }
 }
 
+function updateTaskSlots() {
+  var currentHour = moment().hour();
+
+  $(".time-block").each(function () {
+    var hour = $(this).attr("data-hour");
+
+    if (hour < currentHour) {
+      $(this).find(".description").addClass("past");
+    } else if (hour == currentHour) {
+      $(this).find(".description").addClass("present");
+    } else {
+      $(this).find(".description").addClass("future");
+    }
+  });
+}
+
 function init() {
   generateTimeSlots();
+  updateTaskSlots();
 }
 
 init();
