66e71764b772c2f112aa559cb83a738e8122acd3
VascoMiguens <v_miguens@hotmail.com>
2022-10-27 12:33:01 +0100

    - populate the task row with the item fetched from the local storage - function to store the task items in local storage - function to remove the task items from local storage

--- assets/script.js
+++ assets/script.js
@@ -4,6 +4,8 @@ var options = {
   endHour: 24,
 };
 
+var entry = JSON.parse(localStorage.getItem("entry")) || [];
+
 function generateTimeSlots() {
   //loop through every hour and generate the layout
   for (var hour = options.startHour; hour <= options.endHour; hour++) {
@@ -21,9 +23,7 @@ function generateTimeSlots() {
       )
       .text(moment(hour, "h").format("h A"));
     //task container
-    var taskContainer = $("<div>").addClass(
-      "col-8 col-sm-10 col-md-10 col-xl-10"
-    );
+    var taskContainer = $("<div>").addClass("col-8 col-sm-9 col-md-9 col-xl-9");
     //task description
     var taskSlot = $("<textarea>").addClass("col-md-12 description h-100");
     //save button container
@@ -32,6 +32,21 @@ function generateTimeSlots() {
     );
     //save button
     var saveBtn = $("<i>").addClass("fas fa-save");
+    saveContainer.on("click", onSaveEntry);
+    var removeContainer = $("<div>").addClass(
+      "removeBtn d-flex justify-content-center align-items-center col-1 col-sm-1 col-md-1 col-xl-1"
+    );
+    var removeBtn = $("<i>").addClass("fas fa-trash");
+    removeBtn.on("click", removeEntry);
+
+    var object = JSON.parse(localStorage.getItem("entry")) || [];
+    for (var i = 0; i < object.length; i++) {
+      if (hour == object[i].hour) {
+        taskSlot.text(object[i].task);
+      } else {
+        taskSlot.innerHTML = "";
+      }
+    }
 
     //append elements to parents
     rowContainer.append(hourSlot);
@@ -39,25 +54,68 @@ function generateTimeSlots() {
     taskContainer.append(taskSlot);
     rowContainer.append(saveContainer);
     saveContainer.append(saveBtn);
+    rowContainer.append(removeContainer);
+    removeContainer.append(removeBtn);
     //append the row container to the main container
     $(".container").append(rowContainer);
   }
 }
 
 function updateTaskSlots() {
+  //current time
   var currentHour = moment().hour();
-
   $(".time-block").each(function () {
     var hour = $(this).attr("data-hour");
-
+    //if the data-hour attribute is less than the current hour
     if (hour < currentHour) {
+      //add class ".past"
       $(this).find(".description").addClass("past");
+      //if its equal to the current hour
     } else if (hour == currentHour) {
+      //add class ".present"
       $(this).find(".description").addClass("present");
+      //if its over the current hour
     } else {
+      //add class ".future"
       $(this).find(".description").addClass("future");
     }
   });
+}
+
+function onSaveEntry(e) {
+  //prevent default action
+  e.preventDefault();
+  //get the previous parent children value
+  task = $(e.target).parent().prev().children().val();
+  //get the parent's parent data-hour attribute
+  hour = $(e.target).parent().parent().attr("data-hour");
+
+  //save the hour and task into a object
+  var taskObj = {
+    task: task,
+    hour: hour,
+  };
+  //if what was retrieved from local storage is empty
+  if (entry === null) {
+    //save the entry into local storage
+    localStorage.setItem("entry", JSON.stringify(entry));
+  } else {
+    //push the new object into the array that was retrieved from the local storage
+    entry.push(taskObj);
+    //and save it into local storage
+    localStorage.setItem("entry", JSON.stringify(entry));
+  }
+}
+
+function removeEntry(e) {
+  //get the data-hour attribute stored in the parent's parent element
+  var target = $(e.target).parent().parent().attr("data-hour");
+  //filter out the task to be removed and store it in the temp variable
+  var temp = entry.filter((task) => task.hour != target);
+  //save the temp array into local storage
+  localStorage.setItem("entry", JSON.stringify(temp));
+  //reload window to remove the deleted item from the UI
+  window.location.reload();
 }
 
 function init() {
